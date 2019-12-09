823efddf8cbdfa3fcb8f71762db10acd89923732
Ben Junya <bjunya@gmail.com>
2019-12-08 20:44:57 -0800

    Created a cool looking terminal

--- static/script.js
+++ static/script.js
@@ -1,9 +1,36 @@
-const boxes = Array.from(document.querySelectorAll('.box'));
+const body = document.getElementById('root');
 
-const onBoxClick = ({ currentTarget }) => {
-  currentTarget.classList.toggle('expand');
 
+function clear() {
+  body.innerHTML = '';
+  return Promise.resolve(true);
 }
-boxes.forEach(box => {
-  box.addEventListener('click', onBoxClick);
-})
+
+function fuzzyText(text) {
+  const textarea = document.querySelector('textarea.terminal');
+  textarea.removeEventListener('keyup', onTerminalInput);
+  textarea.parentNode.removeChild(textarea);
+  const p = document.createElement('p');
+  p.classList.add('terminal', 'fullscreen', 'fuzzy');
+  p.innerHTML = text;
+  body.appendChild(p);
+}
+
+function onTerminalInput(event) {
+  if (event.key === 'Enter') {
+    fuzzyText(event.currentTarget.value);
+  }
+}
+
+// Initialize the code!
+function init() {
+  const textarea = document.createElement('textarea');
+  textarea.classList.add('terminal', 'fullscreen');
+  body.appendChild(textarea);
+  textarea.focus();
+  textarea.addEventListener('keyup', onTerminalInput);
+}
+
+
+// Let's get this bad party started
+init();
