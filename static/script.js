c05135c086c033dd63d7d9ee8494c0362f89ca33
Ben Junya <bjunya@gmail.com>
2019-12-24 13:50:04 -0800

    Top and bottom audio visualization is totally working now

--- static/script.js
+++ static/script.js
@@ -1,10 +1,50 @@
-const body = document.getElementById('root');
-const AudioContext = window.AudioContext || window.webkitAudioContext;
+// Create global vars for audio
+const AudioContext = (window.AudioContext || window.webkitAudioContext);
+const audioElement = document.querySelector('audio');
 let audioCtx;
 let track;
 let analyzer;
 
-const audioElement = document.querySelector('audio');
+const body = document.getElementById('root');
+
+/** This value will affect FFT buffer size. Don't set it too high! */
+const TOTAL_ELEMENTS = 512;
+
+const topWrapper = document.createElement('div');
+topWrapper.classList.add('syn', 'top');
+body.appendChild(topWrapper);
+
+for (let i = 0; i < TOTAL_ELEMENTS / 4; i++) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('bar');
+  topWrapper.appendChild(newDiv);
+}
+
+const bottomWrapper = document.createElement('div');
+bottomWrapper.classList.add('syn', 'bottom');
+body.appendChild(bottomWrapper);
+
+for (let i = 0; i < TOTAL_ELEMENTS / 4; i++) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('bar');
+  bottomWrapper.appendChild(newDiv);
+}
+
+
+
+const topElements = Array.from(document.querySelectorAll('.syn.top .bar'));
+const bottomElements = Array.from(document.querySelectorAll('.syn.bottom .bar'));
+// const topDiv = document.createElement('div');
+// topDiv.classList.add('syn', 'top');
+
+// const rightDiv = document.createElement('div');
+// rightDiv.classList.add('syn', 'right');
+
+// const bottomDiv = document.createElement('div');
+// bottomDiv.classList.add('syn', 'bottom');
+
+// const leftDiv = document.createElement('div');
+// leftDiv.classList.add('syn', 'left');
 
 
 let didStart = false;
@@ -13,21 +53,34 @@ function startAudio() {
   audioCtx = new AudioContext();
   track = audioCtx.createMediaElementSource(audioElement);
   analyzer = audioCtx.createAnalyser();
-  analyzer.fftSize = 1024;  
+  analyzer.fftSize = TOTAL_ELEMENTS * 2;
+
   track
-    .connect(analyzer) // This isn't reactive... what do
+    .connect(analyzer)
     .connect(audioCtx.destination);
 
-    audioElement.play();
-    animate();
+  audioElement.play();
+  animate();
 }
 
+let high = 0;
 
 function animate() {
   const bufferLength = analyzer.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
   analyzer.getByteTimeDomainData(dataArray);
-  console.log(dataArray);
+  
+
+  // topDiv.setAttribute('style', `height: ${dataArray[0]}px;`);
+  for (let i = 0; i < topElements.length; i++) {
+    const div = topElements[i];
+    div.setAttribute('style', `height: ${dataArray[i]}px`);
+  }
+  
+  for (let i = 0; i < bottomElements.length; i++) {
+    const div = bottomElements[i];
+    div.setAttribute('style', `height: ${dataArray[i + (TOTAL_ELEMENTS / 4)]}px`);
+  }
   // for (let i = 0; i < bufferLength; i++) {
   //   // canvas stuff goes in here
   //   // console.log(dataArray[i]);
@@ -69,30 +122,18 @@ function onTerminalInput(event) {
 // Initialize the code!
 function init() {
   const textarea = document.createElement('textarea');
-  textarea.classList.add('terminal', 'fullscreen');
+  textarea.classList.add('terminal');
   body.appendChild(textarea);
   textarea.focus();
   textarea.addEventListener('keyup', onTerminalInput);
   
-  const topDiv = document.createElement('div');
-  topDiv.classList.add('syn', 'top');
   
-  const rightDiv = document.createElement('div');
-  rightDiv.classList.add('syn', 'right');
-  
-  const bottomDiv = document.createElement('div');
-  bottomDiv.classList.add('syn', 'bottom');
-  
-  const leftDiv = document.createElement('div');
-  leftDiv.classList.add('syn', 'left');
 
-  body.appendChild(topDiv);
-  body.appendChild(rightDiv);
-  body.appendChild(bottomDiv);
-  body.appendChild(leftDiv);
+  // body.appendChild(topDiv);
+  // body.appendChild(rightDiv);
+  // body.appendChild(bottomDiv);
+  // body.appendChild(leftDiv);
   // Reference: https://www.kkhaydarov.com/audio-visualizer/
 }
 
-
-// Let's get this bad party started
 init();
